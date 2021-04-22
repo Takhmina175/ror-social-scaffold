786b021f70a94c04b1fd6142f720e2fe96907df8
gdumani <gdumani@gmail.com>
2021-04-22 15:52:18 -0600

    fix inverse_friendship association

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
   has_many :friendships
   has_many :friends, through: :friendships
-  has_many :inverse_friendships, class_name: :frienship, foreign_key: :friend_id
+  has_many :inverse_friendships, class_name: 'Friendship', foreign_key: :friend_id
   has_many :inverse_friends, through: :inverse_friendships, source: :user
-  
+
   validates :name, presence: true, length: { maximum: 20 }
 
   has_many :posts
