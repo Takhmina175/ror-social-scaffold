9a1f1afc38ce928b4787d27ed05f43514e59545c
mina <takhminam@gmail.com>
2021-05-13 01:26:03 +0300

    Add notification item to the navbar

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,5 +3,8 @@ module UserHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.to_s.downcase)
     gravatar_url = "http://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'profile-pic')
+  end 
+  def notification(user)
+    user.pending_friends.count + user.requested_friends.count
   end
 end
